936f425aaa7704e4d3daada74f496c17e25d75b2
codesbyree <bahreesaepul1@gmail.com>
2026-03-24 18:17:48 +0700

    feat: Implement theme toggler to the app bar.

--- components/app-bar/app-bar.tsx
+++ components/app-bar/app-bar.tsx
@@ -1,9 +1,14 @@
+import { ThemeToggler } from "../theme-toggler"
 import AppBarBreadcrumb from "./app-bar-breadcrumbs"
 
 export default function AppBar() {
   return (
-    <div className="rounded-xl border border-border bg-card p-4">
+    <div className="flex items-center justify-between gap-2 rounded-xl border border-border bg-card p-4">
       <AppBarBreadcrumb />
+
+      <div className="flex items-center gap-2">
+        <ThemeToggler />
+      </div>
     </div>
   )
 }
